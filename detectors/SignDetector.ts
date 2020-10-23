ea5327131e6185cb6409c22eb2411b0bf5b2a0f8
Emma Goodliffe <emma@piemag.com>
2020-10-23 21:27:52 +0100

    refactored slightly

--- detectors/SignDetector.ts
+++ detectors/SignDetector.ts
@@ -1,4 +1,4 @@
-import { DetectedSign, RGB } from "./common";
+import { DetectedSign, Point, RGB } from "./common";
 import * as conversions from "./conversions";
 import { getMean } from "./helpers";
 import Image from "./Image";
@@ -18,6 +18,45 @@ export default class SignDetector {
   constructor(imagePath: string) {
     this.image = new Image(imagePath);
   }
+  private getMinimumAndMaximum(
+    greyScalePerSignPix: number[],
+    allPixels: {
+      rgb: RGB;
+      point: Point;
+    }[]
+  ): [Point, Point] {
+    let minX = Infinity;
+    let minY = Infinity;
+    for (const i in greyScalePerSignPix) {
+      const greyScale = greyScalePerSignPix[i];
+      const { x, y } = allPixels[i].point;
+      if (greyScale !== -1) {
+        if (x < minX) {
+          minX = x;
+        }
+        if (y < minY) {
+          minY = y;
+        }
+      }
+    }
+    let maxX = 0;
+    let maxY = 0;
+    for (const i in greyScalePerSignPix) {
+      const greyScale = greyScalePerSignPix[i];
+      const { x, y } = allPixels[i].point;
+      if (greyScale !== -1) {
+        if (x > maxX) {
+          maxX = x;
+        }
+        if (y > maxY) {
+          maxY = y;
+        }
+      }
+    }
+    const min = { x: minX, y: minY };
+    const max = { x: maxX, y: maxY };
+    return [min, max];
+  }
   async detect(): Promise<DetectedSign> {
     const pixels = await this.image.getPixels();
     const rgbPerPix = pixels.rgba.map(conversions.RGBAToRGB);
@@ -31,17 +70,17 @@ export default class SignDetector {
       redPixelIndices,
       pixels.width
     );
-    const redXPerPix = redPixels.map(pixel => pixel.x);
-    const redYPerPix = redPixels.map(pixel => pixel.y);
-    const averagePixel = {
-      x: getMean(redXPerPix),
-      y: getMean(redYPerPix),
+    const xPerRedPix = redPixels.map(pixel => pixel.x);
+    const yPerRedPix = redPixels.map(pixel => pixel.y);
+    const averageRedPixel = {
+      x: getMean(xPerRedPix),
+      y: getMean(yPerRedPix),
     };
-    const centreVector = Vector.from(averagePixel);
-    const radiusPerPix = redPixels.map(pixel =>
+    const centreVector = Vector.from(averageRedPixel);
+    const radiusPerRedPix = redPixels.map(pixel =>
       Vector.dist(Vector.from(pixel), centreVector)
     );
-    const minRadius = Math.min(...radiusPerPix);
+    const minRadius = Math.min(...radiusPerRedPix);
     const allPixels = rgbPerPix.map((rgb, i) => {
       const points = conversions.indicesToPoints([i], pixels.width);
       if (points.length !== 1) {
@@ -55,69 +94,37 @@ export default class SignDetector {
     });
     const signGreyScalePerPix: number[] = allPixels.map(pixel => {
       const distance = Vector.dist(Vector.from(pixel.point), centreVector);
-      if (distance < minRadius) {
-        return getMean(pixel.rgb);
-      } else {
-        return -1;
-      }
+      if (distance < minRadius) return getMean(pixel.rgb);
+      return -1;
     });
-    let signMinX = Infinity;
-    let signMinY = Infinity;
-    for (const i in signGreyScalePerPix) {
-      const greyScale = signGreyScalePerPix[i];
-      const { x, y } = allPixels[i].point;
-      if (greyScale !== -1) {
-        if (x < signMinX) {
-          signMinX = x;
-        }
-        if (y < signMinY) {
-          signMinY = y;
-        }
-      }
-    }
-    let signMaxX = 0;
-    let signMaxY = 0;
-    for (const i in signGreyScalePerPix) {
-      const greyScale = signGreyScalePerPix[i];
-      const { x, y } = allPixels[i].point;
-      if (greyScale !== -1) {
-        if (x > signMaxX) {
-          signMaxX = x;
-        }
-        if (y > signMaxY) {
-          signMaxY = y;
-        }
-      }
-    }
-    const signWidth = 1 + signMaxX - signMinX;
-    const signHeight = 1 + signMaxY - signMinY;
+    const [minSign, maxSign] = this.getMinimumAndMaximum(
+      signGreyScalePerPix,
+      allPixels
+    );
+    const signWidth = 1 + maxSign.x - minSign.x;
+    const signHeight = 1 + maxSign.y - minSign.y;
     if (signWidth !== signHeight) {
       throw "Detected sign is not a circle";
     }
-    const signCroppedGreyScalePerPix = signGreyScalePerPix.filter(
-      (greyScale, i) => {
+    const signCroppedGreyScalePerPix = signGreyScalePerPix
+      .filter((greyScale, i) => {
         const { x, y } = allPixels[i].point;
-        const goodX = x >= signMinX && x <= signMaxX;
-        const goodY = y >= signMinY && y <= signMaxY;
+        const goodX = x >= minSign.x && x <= maxSign.x;
+        const goodY = y >= minSign.y && y <= maxSign.y;
         const goodPoint = goodX && goodY;
         return goodPoint;
-      }
-    );
-    const signFixedCroppedGreyScalePerPix = signCroppedGreyScalePerPix.map(
-      greyScale => {
+      })
+      .map(greyScale => {
         if (greyScale === -1) return 255;
         if (greyScale > 255 / 2) return 255;
         return 0;
-      }
-    );
-    const signInvertedFixedCroppedGreyScalePerPix = signFixedCroppedGreyScalePerPix.map(
-      greyScale => 255 - greyScale
-    );
+      })
+      .map(greyScale => 255 - greyScale);
     return {
       points: redPixels,
-      centre: averagePixel,
+      centre: averageRedPixel,
       radius: minRadius,
-      sign: signInvertedFixedCroppedGreyScalePerPix,
+      sign: signCroppedGreyScalePerPix,
     };
   }
 }
