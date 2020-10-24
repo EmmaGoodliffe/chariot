b92d0d3437bd00aefa229ae16ec19142f1156a17
Emma Goodliffe <emma@piemag.com>
2020-10-24 23:17:26 +0100

    ready for tf

--- index.ts
+++ index.ts
@@ -1,12 +1,15 @@
-import { join } from "path";
-import { PathDetector, SignDetector } from "./detectors";
+// import { join } from "path";
+// import { PathDetector, SignDetector } from "./detectors";
+import MNIST from "./recognisers/MNIST";
 
-const detect = async () => {
-  const pathDetector = new PathDetector(join(__dirname, "path.png"));
-  const path = await pathDetector.detect(5);
-  const signDetector = new SignDetector(join(__dirname, "sign.png"));
-  const sign = await signDetector.detect();
-  console.log({ path, sign });
-};
+new MNIST();
 
-detect().catch(console.error);
+// const detect = async () => {
+//   const pathDetector = new PathDetector(join(__dirname, "path.png"));
+//   const path = await pathDetector.detect(5);
+//   const signDetector = new SignDetector(join(__dirname, "sign.png"));
+//   const sign = await signDetector.detect();
+//   console.log({ path, sign });
+// };
+
+// detect().catch(console.error);
