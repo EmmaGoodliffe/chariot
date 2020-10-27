c2f050094ad0d219c351f4d3ffae2d00b81e7077
Emma Goodliffe <emma@piemag.com>
2020-10-27 12:58:48 +0000

    basic loading done

--- helpers.ts
+++ helpers.ts
@@ -6,11 +6,24 @@ export const getMean = (arr: number[]): number => {
   return total / arr.length;
 };
 
-export const chunk = <T>(arr: T[], n: number): T[][] => {
-  const results: T[][] = [];
+// export const chunk = <T>(arr: T[], n: number): T[][] => {
+//   const results: T[][] = [];
+//   for (let i = 0; i < arr.length; i += n) {
+//     const result = arr.slice(i, i + n);
+//     results.push(result);
+//   }
+//   return results;
+// };
+
+type ArrayOrBytes<T> = T[] | Uint8Array;
+
+export function chunk<T>(arr: T[], n: number): T[][];
+export function chunk(arr: Uint8Array, n: number): Uint8Array[];
+export function chunk<T>(arr: ArrayOrBytes<T>, n: number): ArrayOrBytes<T>[] {
+  const results: ArrayOrBytes<T>[] = [];
   for (let i = 0; i < arr.length; i += n) {
     const result = arr.slice(i, i + n);
     results.push(result);
   }
   return results;
-};
+}
--- recognisers/QuickDraw.ts
+++ recognisers/QuickDraw.ts
@@ -1,79 +1,26 @@
-import fs from "fs";
-import nd from "ndjson";
+import { readFileSync } from "fs";
 import { join } from "path";
-import { Point } from "../common";
+import { chunk } from "../helpers";
 import Image from "../Image";
 
 const data = {
-  width: 256,
+  byteOffset: 80,
+  width: 28,
 } as const;
 
-type Stroke = [number[], number[]];
-
-interface Sketch {
-  word: string;
-  countrycode: string;
-  timestamp: string;
-  recognized: boolean;
-  key_id: string;
-  drawing: Stroke[];
-}
-
-const readSketches = (category: string): Promise<Sketch[]> =>
-  new Promise((resolve, reject) => {
-    const path = join(__dirname, `QuickDraw/data/${category}.ndjson`);
-    const sketches: Sketch[] = [];
-    fs.createReadStream(path)
-      .pipe(nd.parse())
-      .on("data", sketch => sketches.push(sketch))
-      .on("error", reject)
-      .on("end", () => resolve(sketches));
-  });
-
-const strokeToPoints = (stroke: Stroke) => {
-  const [xs, ys] = stroke;
-  if (xs.length !== ys.length) {
-    throw "Different number of x and y values in stroke";
-  }
-  const points: Point[] = [];
-  for (const i in xs) {
-    const x = xs[i];
-    const y = ys[i];
-    points.push({ x, y });
-  }
-  return points;
-};
-
 const run = async () => {
-  console.time("read");
-  const sketches = await readSketches("traffic light");
-  console.timeEnd("read");
-  console.time("points");
-  const pointSketches = [sketches[0]].map(sketch => ({
-    ...sketch,
-    points: sketch.drawing.map(strokeToPoints).flat(),
-  }));
-  console.timeEnd("points");
-  console.time("images");
-  const imageSketches = pointSketches.map(sketch => {
-    const { points } = sketch;
-    const image = Array<number>(data.width ** 2).fill(0);
-    for (const point of points) {
-      const { x, y } = point;
-      const i = y * data.width + x;
-      image[i] = 255;
-    }
-    return {
-      ...sketch,
-      image,
-    };
-  });
-  console.timeEnd("images");
-  console.time("save");
-  const { image } = imageSketches[0];
-  const png = await Image.imageToPNG(image, data.width, data.width);
+  const raw = readFileSync(
+    join(__dirname, "./QuickDraw/data/traffic light.npy")
+  );
+  const bytes = new Uint8Array(raw);
+  const body = bytes.slice(data.byteOffset);
+  const images = chunk(body, data.width ** 2);
+  const png = await Image.imageToPNG(
+    Array.from(images[0]),
+    data.width,
+    data.width
+  );
   await png.writeAsync("./traffic light.png");
-  console.timeEnd("save");
 };
 
 run().catch(console.error);
