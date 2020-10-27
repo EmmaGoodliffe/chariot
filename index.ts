75443c0afad46479733b370622af7abeb4cc3e31
Emma Goodliffe <emma@piemag.com>
2020-10-27 09:14:17 +0000

    moved PNG's to assets folder

--- index.ts
+++ index.ts
@@ -5,10 +5,10 @@ import { MNIST } from "./recognisers";
 const mnist = new MNIST();
 
 const run = async () => {
-  const pathDetector = new PathDetector(join(__dirname, "path.png"));
+  const pathDetector = new PathDetector(join(__dirname, "assets/path.png"));
   const path = await pathDetector.detect(5);
   console.log(path.path.length);
-  const signDetector = new SignDetector(join(__dirname, "sign.png"));
+  const signDetector = new SignDetector(join(__dirname, "assets/sign.png"));
   const sign = await signDetector.detect();
   const image = await MNIST.prepareImage(sign.sign);
   const nn = await mnist.nn.load(
