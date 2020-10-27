7edd55b484ababc5c25394063fef7c1650a41639
Emma Goodliffe <emma@piemag.com>
2020-10-27 09:49:11 +0000

    converted stroke to points

--- recognisers/QuickDraw.ts
+++ recognisers/QuickDraw.ts
@@ -1,6 +1,7 @@
 import fs from "fs";
 import nd from "ndjson";
 import { join } from "path";
+import { Point } from "../common";
 
 type Stroke = [number[], number[]];
 
@@ -24,9 +25,27 @@ const readSketches = (category: string): Promise<Sketch[]> =>
       .on("end", () => resolve(sketches));
   });
 
+const strokeToPoints = (stroke: Stroke) => {
+  const [xs, ys] = stroke;
+  if (xs.length !== ys.length) {
+    throw "Different number of x and y values in stroke";
+  }
+  const points: Point[] = [];
+  for (const i in xs) {
+    const x = xs[i];
+    const y = ys[i];
+    points.push({ x, y });
+  }
+  return points;
+};
+
 const run = async () => {
   const sketches = await readSketches("traffic light");
-  console.log(sketches[0].drawing[0], sketches[0].drawing[0]);
+  const pointSketches = sketches.map(sketch => ({
+    ...sketch,
+    drawing: sketch.drawing.map(strokeToPoints),
+  }));
+  console.log(pointSketches[0].drawing[0]);
 };
 
 run().catch(console.error);
